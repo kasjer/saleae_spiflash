dc40d748500a89e6a5351ecea5eab3489785af56
Jerzy Kasenberg <jerzy.kasenberg@gmail.com>
2017-11-25 11:06:08 +0100

    Add version header

---
+++ vc2017/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#ifndef VERSION_MAJOR
+#define VERSION_MAJOR   1
+#endif
+#ifndef VERSION_MINOR
+#define VERSION_MINOR   0
+#endif
+#ifndef VERSION_3
+#define VERSION_3       3
+#endif
+#ifndef VERSION_4
+#define VERSION_4       0
+#endif
+
+#ifndef VERSION_STRING
+#define VERSION_STRING  "1.0.3.0"
+#endif
+
+#endif
